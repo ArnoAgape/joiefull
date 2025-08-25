a6de5abdac6cd65512c3b2ace72f9873427914f2
Arno Bouiron <arno.bouiron@gmail.com>
2025-08-25 15:50:59 +0200

    design finished on detailsscreen

--- app/src/main/java/com/openclassrooms/joiefull/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/openclassrooms/joiefull/ui/details/DetailsScreen.kt
@@ -3,6 +3,7 @@ package com.openclassrooms.joiefull.ui.details
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,25 +14,34 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -266,6 +276,48 @@ fun ArticleDetailsContent(
                 style = MaterialTheme.typography.bodyMedium
             )
         }
+        Spacer(modifier = Modifier.height(6.dp))
+        Row(
+            modifier = Modifier.padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_profile),
+                contentDescription = stringResource(R.string.profile_picture),
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+            // RatingBar
+            var rating by remember { mutableIntStateOf(0) }
+
+            Row {
+                for (i in 1..5) {
+                    Icon(
+                        imageVector = if (i <= rating) Icons.Default.Star else Icons.Outlined.Star,
+                        contentDescription = null,
+                        tint = if (i <= rating) Color(0xFFFFC107) else Color.Gray,
+                        modifier = Modifier
+                            .size(45.dp)
+                            .clickable { rating = i }
+                    )
+                }
+            }
+        }
+        Column(modifier = Modifier.padding(8.dp)) {
+
+            var text by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(stringResource(R.string.comment)) },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(10.dp)
+            )
+        }
     }
 }
 
@@ -284,7 +336,7 @@ fun ArticleDetailsPreview() {
         ),
         Category.SHOES
     )
-    ArticleDetailsContent(fakeArticle, onBackClick = {}, onShareClick = {}, favorite = false, onFavoriteClick = {} )
+    ArticleDetailsContent(fakeArticle, onBackClick = {}, onShareClick = {}, favorite = false, onFavoriteClick = {})
 }
 
 
