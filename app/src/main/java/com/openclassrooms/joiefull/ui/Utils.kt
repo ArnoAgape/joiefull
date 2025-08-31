c520578fd25e4dee634a6ad97d4fa30215459f3e
Arno <arno.bouiron@gmail.com>
2025-08-31 21:50:28 +0530

    accessibility updated

---
+++ app/src/main/java/com/openclassrooms/joiefull/ui/Utils.kt
@@ -0,0 +1,16 @@
+package com.openclassrooms.joiefull.ui
+
+object Utils {
+
+    fun formatPriceForAccessibility(price: Double): String {
+        val euros = price.toInt()
+        val cents = ((price - euros) * 100).toInt()
+
+        return if (cents == 0) {
+            "$euros euros"
+        } else {
+            "$euros euros $cents"
+        }
+    }
+
+}
